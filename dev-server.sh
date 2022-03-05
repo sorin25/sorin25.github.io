a34a63a2460396d425e999e3ca36986441b851e4
Sorin Adrian Savu <sorin@itguy.ro>
2022-03-05 12:58:46 +0200

    fixed startup script

--- dev-server.sh
+++ dev-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-WORKDIR=$(realpath $(dirname $0)/docs)
-docker run --name blog_dev --rm --volume="$WORKDIR:/srv/jekyll" --publish 5000:5000 -d \
+WORKDIR=$(realpath $(dirname $0)/)
+docker run --name blog_dev --volume="$WORKDIR:/srv/jekyll" --publish 4000:4000 -d \
   jekyll/jekyll jekyll serve
 
