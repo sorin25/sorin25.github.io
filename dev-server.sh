fb04c0a89e4904053de520671856017ae9870ccc
Sorin Adrian Savu <sorin@itguy.ro>
2022-03-06 13:20:33 +0200

    remove container before starting

--- dev-server.sh
+++ dev-server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 WORKDIR=$(realpath $(dirname $0)/)
+docker rm blog_dev
+
 docker run --name blog_dev --volume="$WORKDIR:/srv/jekyll" --publish 4000:4000 -d \
   jekyll/jekyll jekyll serve
 
